dc1446584c2e303f7a912ccee4e7409098176541
Jonatan Thunell <jonatan.thunell@yahoo.com>
2024-10-04 17:26:21 +0200

    Moved User to separate file and cleaned up

---
+++ LabbBlazor/User.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LabbBlazor
+{
+    public class User
+    {
+        public int ID { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string? Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+        [Required]
+        public Address Address { get; set; }
+        [Required]
+        public Company Company { get; set; }
+        public User()
+        {
+            Address = new Address();
+            Company = new Company();
+        }
+    }
+    public class Address
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string? Street { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string? City { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string? ZipCode { get; set; }
+    }
+    public class Company
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string? Name { get; set; }
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        public string? CatchPhrase { get; set; }
+    }
+}
