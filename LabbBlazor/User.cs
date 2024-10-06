f1c8b16ba0cd32586aaba65cc7dc13f9006de95c
Jonatan Thunell <jonatan.thunell@yahoo.com>
2024-10-06 09:59:40 +0200

    Added sample user data and User constructors

--- LabbBlazor/User.cs
+++ LabbBlazor/User.cs
@@ -20,6 +20,15 @@ namespace LabbBlazor
             Address = new Address();
             Company = new Company();
         }
+        public User(int id, string name, string email, string streetAddress, string cityAddress, string zipcodeAddress, string nameCompany, string catchphraseCompany)
+        {
+            ID = id;
+            Name = name;
+            Email = email;
+            Address = new Address(streetAddress, cityAddress, zipcodeAddress);
+            Company = new Company(nameCompany, catchphraseCompany);
+
+        }
     }
     public class Address
     {
@@ -32,6 +41,16 @@ namespace LabbBlazor
         [Required]
         [StringLength(50, MinimumLength = 2)]
         public string? ZipCode { get; set; }
+        public Address()
+        {
+            
+        }
+        public Address(string street, string city, string zipcode)
+        {
+            Street = street;
+            City = city;
+            ZipCode = zipcode;
+        }
     }
     public class Company
     {
@@ -41,5 +60,14 @@ namespace LabbBlazor
         [Required]
         [StringLength(100, MinimumLength = 2)]
         public string? CatchPhrase { get; set; }
+        public Company()
+        {
+            
+        }
+        public Company(string name, string catchphrase)
+        {
+            Name = name;
+            CatchPhrase = catchphrase;
+        }
     }
 }
