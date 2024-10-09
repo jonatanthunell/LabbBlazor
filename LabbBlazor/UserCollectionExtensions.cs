baa50756c08f17d651848fd148c264efdcc8e0af
Jonatan Thunell <jonatan.thunell@yahoo.com>
2024-10-09 19:29:44 +0200

    Design Changes

---
+++ LabbBlazor/UserCollectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace LabbBlazor
+{
+    public static class UserCollectionExtensions
+    {
+        public static List<User> SortByName(this List<User> users)
+        {
+            return users.OrderBy(x => x.Name).ToList();
+        }
+        public static List<User> SortById(this List<User> users)
+        {
+            return users.OrderBy(x => x.ID).ToList();
+        }
+        public static List<User> SearchByName(this List<User> users, string searchTerm)
+        {
+            return users.Where(x => x.Name!.ToLower().Contains(searchTerm.ToLower().Trim())).ToList();
+        }
+        public static List<User> SearchByCity(this List<User> users, string searchTerm)
+        {
+            return users.Where(x => x.Address.City!.ToLower().Contains(searchTerm.ToLower().Trim())).ToList();
+        }
+        public static List<User> FilterByAmount(this List<User> users, int amount)
+        {
+            return users.Take(amount).ToList();
+        }
+        
+    }
+}
+
