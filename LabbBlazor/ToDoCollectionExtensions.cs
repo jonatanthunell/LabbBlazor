bd1516416aedc6866a229c36526ddf895782e985
Jonatan Thunell <jonatan.thunell@yahoo.com>
2024-10-10 11:06:43 +0200

    added toto extensions

---
+++ LabbBlazor/ToDoCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using LabbBlazor.Components.Pages;
+
+namespace LabbBlazor
+{
+    public static class ToDoCollectionExtensions
+    {
+        public static List<ToDo> GetSpecificUserTodos(this List<ToDo> allTodos, User specificUser)
+        {
+            return allTodos.Where(x => x.UserId == specificUser.ID).ToList();
+        }
+        public static List<ToDo> GetIncompletedTodos(this List<ToDo> todos)
+        {
+            return todos.Where(x => !x.Completed).ToList();
+        }
+        public static List<ToDo> FilterTodosByTitle(this List<ToDo> todos, string searchTerm)
+        {
+            return todos.Where(x => x.Title.ToLower().Contains(searchTerm.ToLower().Trim())).ToList();
+        }
+    }
+}
