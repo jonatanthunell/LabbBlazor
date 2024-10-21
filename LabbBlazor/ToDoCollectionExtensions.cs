e1f56bda19032446e0b9296174e1861bd2d8a7ba
Jonatan Thunell <jonatan.thunell@yahoo.com>
2024-10-21 08:48:58 +0200

    Minor changes

--- LabbBlazor/ToDoCollectionExtensions.cs
+++ LabbBlazor/ToDoCollectionExtensions.cs
@@ -2,7 +2,7 @@
 {
     public static class ToDoCollectionExtensions
     {
-        public static List<ToDo> GetSpecificUserTodos(this List<ToDo> allTodos, int specificUser) => allTodos.Where(x => x.UserId == specificUser).ToList();
+        public static List<ToDo> GetCurrentUserTodos(this List<ToDo> todos, int currentUserId) => todos.Where(x => x.UserId == currentUserId).ToList();
         public static List<ToDo> GetIncompletedTodos(this List<ToDo> todos) => todos.Where(x => !x.Completed).ToList();
         public static List<ToDo> FilterTodosByTitle(this List<ToDo> todos, string searchTerm) => todos.Where(x => x.Title.ToLower().Contains(searchTerm.ToLower().Trim())).ToList();
     }
